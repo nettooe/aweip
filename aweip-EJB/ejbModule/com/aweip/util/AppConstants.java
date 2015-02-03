ccb11ae28fa10e05bd848652e4e9962673838fbb
Óliver Emanuel <netto.oe@gmail.com>
2015-02-02 23:05:01 -0200

    Adicionada classe contendo constantes do sistema;

---
+++ aweip-EJB/ejbModule/com/aweip/util/AppConstants.java
@@ -0,0 +1,14 @@
+package com.aweip.util;
+
+/**
+ * The Class AppConstants.
+ */
+public class AppConstants {
+	
+	/** The senha tamanho minimo. */
+	public final static int SENHA_TAMANHO_MINIMO = 6; 
+	
+	/** The senha tamanho maximo. */
+	public final static int SENHA_TAMANHO_MAXIMO = 15;
+
+}
